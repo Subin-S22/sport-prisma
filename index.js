5d187a13e868e26f969ca4e8eadae3e0829445f9
Subin-S22 <subin.s22@outlook.com>
2024-04-03 17:30:41 +0530

    schema

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import router from "./routes/index.js";
+import bodyParser from "body-parser";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allSports = await prisma.sport.findMany();
+  console.log(allSports);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+const app = new express();
+
+//accept the application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//router for the api
+app.use("/api", router);
+
+app.listen(4321, () => {
+  console.log("app is listening to 4321");
+});
